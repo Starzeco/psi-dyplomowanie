3dc89b66117250967902aaa5ceb7dec65ac341bc
MARCEL <marcelkrako@gmail.com>
2022-01-08 11:28:49 +0100

    init backend

---
+++ backend/src/main/kotlin/com/example/dyplomowaniebackend/DyplomowanieBackendApplication.kt
@@ -0,0 +1,11 @@
+package com.example.dyplomowaniebackend
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DyplomowanieBackendApplication
+
+fun main(args: Array<String>) {
+	runApplication<DyplomowanieBackendApplication>(*args)
+}
---
+++ backend/src/test/kotlin/com/example/dyplomowaniebackend/DyplomowanieBackendApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.dyplomowaniebackend
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DyplomowanieBackendApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
