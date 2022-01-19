8858645572dcbc8cd9b15ee5796fee7abe25994e
Jakub Gartska <242421@student.pwr.edu.pl>
2022-01-19 14:16:09 +0100

    implement default states

--- frontend/src/app/components/components.module.ts
+++ frontend/src/app/components/components.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { TranslateModule } from '@ngx-translate/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatToolbarModule } from '@angular/material/toolbar';
@@ -8,11 +9,16 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatDividerModule } from '@angular/material/divider';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatSelectModule } from '@angular/material/select';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { RouterModule } from "@angular/router";
-import { SidenavComponent } from './sidenav/sidenav.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { SidenavComponent } from './sidenav/sidenav.component';
 import { ToolbarComponent } from './toolbar/toolbar.component';
-import { TranslateModule } from '@ngx-translate/core';
+import {
+  LoadingStateComponent,
+} from './loading-state/loading-state.component';
+import { ErrorStateComponent } from './error-state/error-state.component';
+import { EmptyStateComponent } from './empty-state/empty-state.component';
 
 const MAT_MODULES = [
   MatSidenavModule,
@@ -22,17 +28,24 @@ const MAT_MODULES = [
   MatDividerModule,
   MatFormFieldModule,
   MatSelectModule,
+  MatProgressSpinnerModule,
 ];
 
 @NgModule({
   declarations: [
     SidenavComponent,
     ToolbarComponent,
+    LoadingStateComponent,
+    ErrorStateComponent,
+    EmptyStateComponent,
   ],
   exports: [
     SidenavComponent,
     ToolbarComponent,
     TranslateModule,
+    LoadingStateComponent,
+    ErrorStateComponent,
+    EmptyStateComponent,
   ],
   imports: [
     CommonModule,
@@ -44,5 +57,6 @@ const MAT_MODULES = [
     ...MAT_MODULES,
   ]
 })
+
 export class ComponentsModule {
 }
